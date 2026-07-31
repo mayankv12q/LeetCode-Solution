8c122f8f8bfee6015afcc4bce496bd533c6a165c
MayankVerma <mayank843582@gmail.com>
2026-08-01 01:23:47 +0530

    [442] [Time Beats: 100.00%] [Memory Beats: 62.39%] - LeetPush

---
+++ 442-find-all-duplicates-in-an-array/442-Find-All-Duplicates-in-an-Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> dup ;
+
+        for(int i = 0 ; i < nums.size() ;i++){
+            int index = abs(nums[i]) - 1;
+
+            if(nums[index] < 0){
+                dup.push_back(abs(nums[i])) ;
+            }
+            else{
+                nums[index] = -nums[index];
+            }
+        }
+        return dup ;
+    }
+};
