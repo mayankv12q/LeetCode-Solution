1600575347153dce2aed87084892f2ac489668a2
MayankVerma <mayank843582@gmail.com>
2026-08-01 00:49:04 +0530

    [440] [Time Beats: 100.00%] [Memory Beats: 46.25%] - LeetPush

---
+++ 440-k-th-smallest-in-lexicographical-order/440-K-th-Smallest-in-Lexicographical-Order.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long Count(long n,long curr,long next){
+        long steps = 0;
+        while(curr<=n){
+            steps += min(n+1,next) - curr;
+
+            curr *= 10;
+            next *= 10;
+        }
+
+        return steps;
+    }
+
+    int findKthNumber(int n, int k) {
+        long curr = 1;
+        k--;
+
+        while(k>0){
+            long steps = Count(n,curr,curr+1);
+
+            if(steps <= k){
+                curr++;
+                k -= steps ;
+            }
+            else{
+                curr *= 10;
+                k--;
+            }
+        }
+        return curr;
+    }
+};
