108cd5a9eb39ba4af622be6b7d8a4c43881a597f
MayankVerma <mayank843582@gmail.com>
2026-08-08 00:41:16 +0530

    [28] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -4,6 +4,56 @@ public:
         int n = haystack.length();
         int m = needle.length();
 
+        if(m == 0) return 0;
+        if(m > n) return -1;
+
+        vector<int> lps(m,0);
+        int prelps = 0;
+        int i = 1;
+
+        while(i<m){
+            if(needle[i] == needle[prelps]){
+                lps[i++] = ++prelps;
+            }
+            else if(prelps == 0){
+                lps[i++] = 0;
+            }
+            else{
+                prelps = lps[prelps - 1];
+            }
+        }
+
+        i = 0;
+        int j = 0;
+        
+        while(i<n){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+            }
+            else{
+                if(j == 0 ) i++;
+                else  j = lps[j-1] ;
+            }
+
+            if(j == m){
+                return i-m ;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+/* O(M*N)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
         if(m > n) return -1;
 
         for(int i = 0 ; i <= n-m ; i++){
@@ -19,3 +69,4 @@ public:
         return -1;
     }
 };
+*/
