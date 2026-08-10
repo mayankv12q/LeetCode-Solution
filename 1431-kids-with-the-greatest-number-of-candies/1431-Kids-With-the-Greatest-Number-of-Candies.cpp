b4b887747fab0662e89ec34a585388e65f821da7
MayankVerma <mayank843582@gmail.com>
2026-08-10 22:27:48 +0530

    [1431] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- 1431-kids-with-the-greatest-number-of-candies/1431-Kids-With-the-Greatest-Number-of-Candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-Kids-With-the-Greatest-Number-of-Candies.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        //int maxCandies = *max_element(candies.begin(), candies.end());
         int maxCan = 0 ;
         for(int can : candies){
-            if(can >= maxCan){
-                maxCan = can;
-            }
+            maxCan = max(maxCan,can);
         }
 
         vector<bool> res ;
