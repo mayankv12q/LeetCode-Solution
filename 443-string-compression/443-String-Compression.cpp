2119a018c7b0ce461b914e7450f1eebd4d85958b
MayankVerma <mayank843582@gmail.com>
2026-08-14 02:03:57 +0530

    [443] [Time Beats: 100.00%] [Memory Beats: 76.67%] - LeetPush

--- 443-string-compression/443-String-Compression.cpp
+++ 443-string-compression/443-String-Compression.cpp
@@ -4,7 +4,7 @@ public:
        int n = chars.size() ;
        int idx = 0;
         for(int i = 0 ;i<n;i++){
-           char ch = chars[i];
+            char ch = chars[i];
             int count = 0 ;
 
             while(i<n && chars[i] == ch){
